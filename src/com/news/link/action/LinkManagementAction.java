998523100c300d8586161c378bf59e16605a8789
ystartor <ystartor@gmail.com>
2018-04-18 22:39:20 +0800

    测试

--- src/com/news/link/action/LinkManagementAction.java
+++ src/com/news/link/action/LinkManagementAction.java
@@ -1,7 +1,9 @@
 package com.news.link.action;
 /**
- * 
- * @author Administrator
+ * class 链接管理
+ * @author JXX
+ * @date 18/04/18
+ * @modify 修改 JXX 18/04/18
  *
  */
 public class LinkManagementAction {
